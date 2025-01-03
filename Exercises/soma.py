4bf5fccd654efd33d242b3b038b5059613f9cea3
amandasilvamata <166837389+amandasilvamata@users.noreply.github.com>
2025-01-02 22:38:53 -0300

    Create soma.py

---
+++ Exercises/soma.py
@@ -0,0 +1,6 @@
+# Faça um Programa que peça dois números e imprima a soma.
+
+numero1 = int(input("Digite um número: "))
+numero2 = int(input("Digite outro número: "))
+
+print(f"A soma desses valores é {numero1 + numero2}")
